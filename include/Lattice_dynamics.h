fe1ea631dc6f7d511e269d9a0fd8d9cc460c8126
kratman <kratmandu@gmail.com>
2016-03-29 01:06:45 -0400

    Addin some dummy files

---
+++ include/Lattice_dynamics.h
@@ -0,0 +1,10 @@
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+#include <string>
+#include <complex>
+#include <cmath>
+#include <fstream>
+#include <vector>
+using namespace std;
+
---
+++ tests/README.md
@@ -0,0 +1,9 @@
+[//]: # (Mixture of GitHub markdown and HTML. HTML is needed for formatting.)
+
+<div align=center> <h2>
+Lattice Dynamics Documentation
+</h2> </div>
+
+### Introduction
+
+
